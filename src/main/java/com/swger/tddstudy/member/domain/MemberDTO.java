c8c7952c36cf3089e09aa4389b0f5e7610728f86
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2023-07-27 17:15:57 +0900

    DTO로 받아오는 환경 설정을 위해 MemberDTO 생성

---
+++ src/main/java/com/swger/tddstudy/member/domain/MemberDTO.java
@@ -0,0 +1,12 @@
+package com.swger.tddstudy.member.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class MemberDTO {
+
+    private String username;
+    private String password;
+    private String nickname;
+}
