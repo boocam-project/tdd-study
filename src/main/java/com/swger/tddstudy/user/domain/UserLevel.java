b19dc3b0abea46a50a02b799a150196cac52763a
Nine-JH <rnwkgus2@gmail.com>
2023-07-29 18:50:57 +0900

    [feat] 유저 등급 업 기능 추가 * 등급 업 기능 추가 * 반드시 한단계씩 등급 업을 할 수 있다.

--- src/main/java/com/swger/tddstudy/user/domain/UserLevel.java
+++ src/main/java/com/swger/tddstudy/user/domain/UserLevel.java
@@ -1,11 +1,27 @@
 package com.swger.tddstudy.user.domain;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum UserLevel {
 
-    BRONZE("브론즈"), SILVER("실버"), GOLD("골드");
+    BRONZE("브론즈", 0), SILVER("실버", 1), GOLD("골드", 2);
 
     private final String text;
+    private final int level;
+
+    UserLevel levelUp() {
+        UserLevel[] levels = values();
+
+        if (!isMaxLevel()) {
+            return levels[this.level + 1];
+        }
+
+        return this;
+    }
+
+    private boolean isMaxLevel() {
+        return this.level >= values().length - 1;
+    }
 }
