bd92eba42948bdc9b9ffe950600f4c3ce36ba625
Nine-JH <rnwkgus2@gmail.com>
2023-07-29 18:45:44 +0900

    [refactor] user table 명 변경 * user의 경우 MySQL 예약어에 속하기 때문에 users로 네이밍을 변경했습니다.

--- src/main/java/com/swger/tddstudy/user/domain/User.java
+++ src/main/java/com/swger/tddstudy/user/domain/User.java
@@ -4,17 +4,22 @@ import com.swger.tddstudy.util.BaseEntity;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
 @Entity
 public class User extends BaseEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String username;
@@ -28,4 +33,25 @@ public class User extends BaseEntity {
 
     @Enumerated(EnumType.STRING)
     private UserType type;
+
+    private User(String username, String password, String nickname, UserLevel userLevel,
+        UserType type) {
+        this.username = username;
+        this.password = password;
+        this.nickname = nickname;
+        this.userLevel = userLevel;
+        this.type = type;
+    }
+
+    public static User createNewUser(String username, String password, String nickname) {
+        return new User(username, password, nickname, UserLevel.BRONZE, UserType.USER);
+    }
+
+    public static User createNewAdmin(String username, String password, String nickname) {
+        return new User(username, password, nickname, UserLevel.BRONZE, UserType.ADMIN);
+    }
+
+    public void upgradeLevel() {
+        this.userLevel = this.userLevel.levelUp();
+    }
 }
