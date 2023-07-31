e2fe0d2ff36a8a7f48ced6968a3fc423f6ba2fa7
JeongUijeong <94631526+JeongUijeong@users.noreply.github.com>
2023-07-31 14:45:12 +0900

    [test] : 회원가입 및 로그인 실패 테스트 추가

--- src/test/java/com/swger/tddstudy/user/controller/UserControllerTest.java
+++ src/test/java/com/swger/tddstudy/user/controller/UserControllerTest.java
@@ -3,6 +3,7 @@ package com.swger.tddstudy.user.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.swger.tddstudy.user.domain.UserVO;
 import com.swger.tddstudy.user.restController.UserRestController;
+import com.swger.tddstudy.user.service.UserRegex;
 import com.swger.tddstudy.user.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -12,6 +13,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
@@ -28,6 +32,9 @@ public class UserControllerTest {
 
     @MockBean
     UserService userService;
+
+    @MockBean
+    UserRegex userRegex;
 
     // 테스트를 위한 객체 생성 메서드
     // 로그인 테스트용  UserVO 객체
@@ -48,6 +55,22 @@ public class UserControllerTest {
         return new UserVO(1L, "테스트용 이름2", "테스트용 비밀번호2", "테스트용 닉네임2", "SILVER", "USER");
     }
 
+    // 회원가입 정규식 확인 결과 HashMap 객체 (True)
+    public Map<String, Object> newRegexTestTrue() {
+        Map<String, Object> regexTest = new HashMap<>();
+        regexTest.put("result", true);
+        //regexTest.put("message", "양식 안맞음");
+        return regexTest;
+    }
+
+    // 회원가입 정규식 확인 결과 HashMap 객체 (False)
+    public Map<String, Object> newRegexTestFalse() {
+        Map<String, Object> regexTest = new HashMap<>();
+        regexTest.put("result", false);
+        regexTest.put("message", "양식에 대한 설명");
+        return regexTest;
+    }
+
     // 로그인 테스트를 위한 회원 저장
     @BeforeEach
     void beforeAll() {
@@ -55,11 +78,15 @@ public class UserControllerTest {
     }
 
     @Test
-    @DisplayName("회원가입 테스트")
-    void joinTest() throws Exception {
+    @DisplayName("회원가입 테스트 (양식 맞음)")
+    void joinTest1() throws Exception {
 
         given(userService.save(newUser3())).willReturn(
                 newUser4()
+        );
+
+        given(userRegex.isJoinRegex(newUser3())).willReturn(
+                newRegexTestTrue()
         );
 
         String json = new ObjectMapper().writeValueAsString(newUser3());
@@ -68,12 +95,12 @@ public class UserControllerTest {
                         .content(json)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").exists())
-                .andExpect(jsonPath("$.username").exists())
-                .andExpect(jsonPath("$.password").exists())
-                .andExpect(jsonPath("$.nickname").exists())
-                .andExpect(jsonPath("$.userLevel").exists())
-                .andExpect(jsonPath("$.type").exists())
+                .andExpect(jsonPath("$.data.id").exists())
+                .andExpect(jsonPath("$.data.username").exists())
+                .andExpect(jsonPath("$.data.password").exists())
+                .andExpect(jsonPath("$.data.nickname").exists())
+                .andExpect(jsonPath("$.data.userLevel").exists())
+                .andExpect(jsonPath("$.data.type").exists())
                 .andDo(print());
 
         verify(userService).save(newUser3());
@@ -81,8 +108,34 @@ public class UserControllerTest {
     }
 
     @Test
-    @DisplayName("로그인 테스트")
-    void loginTest() throws Exception {
+    @DisplayName("회원가입 테스트 (양식 틀림)")
+    void joinTest2() throws Exception {
+
+        given(userService.save(newUser3())).willReturn(
+                newUser4()
+        );
+
+        given(userRegex.isJoinRegex(newUser3())).willReturn(
+                newRegexTestFalse()
+        );
+
+        String json = new ObjectMapper().writeValueAsString(newUser3());
+
+        mockMvc.perform(post("/api/user/join")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").exists())
+                .andDo(print());
+
+        verify(userRegex).isJoinRegex(newUser3());
+
+    }
+
+
+    @Test
+    @DisplayName("로그인 테스트 (이름, 비밀번호 일치)")
+    void loginTest1() throws Exception {
 
         UserVO loginUser = new UserVO();
         loginUser.setUsername("테스트용 이름1");
@@ -98,12 +151,37 @@ public class UserControllerTest {
                         .content(json)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").exists())
-                .andExpect(jsonPath("$.username").exists())
-                .andExpect(jsonPath("$.password").exists())
-                .andExpect(jsonPath("$.nickname").exists())
-                .andExpect(jsonPath("$.userLevel").exists())
-                .andExpect(jsonPath("$.type").exists())
+                .andExpect(jsonPath("$.data.id").exists())
+                .andExpect(jsonPath("$.data.username").exists())
+                .andExpect(jsonPath("$.data.password").exists())
+                .andExpect(jsonPath("$.data.nickname").exists())
+                .andExpect(jsonPath("$.data.userLevel").exists())
+                .andExpect(jsonPath("$.data.type").exists())
+                .andDo(print());
+
+        verify(userService).login(loginUser);
+
+    }
+
+    @Test
+    @DisplayName("로그인 테스트 (이름, 비밀번호 불일치)")
+    void loginTest2() throws Exception {
+
+        UserVO loginUser = new UserVO();
+        loginUser.setUsername("테스트용 이름1");
+        loginUser.setPassword("테스트용 비밀번호2");
+
+        given(userService.login(loginUser)).willReturn(
+                null
+        );
+
+        String json = new ObjectMapper().writeValueAsString(loginUser);
+
+        mockMvc.perform(post("/api/user/login")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.message").exists())
                 .andDo(print());
 
         verify(userService).login(loginUser);
