c2d8b75f39608c3622f2c8621113b752ef569820
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2023-07-31 16:48:27 +0900

    Mock을 이용한 Controller Test

--- src/main/java/com/swger/tddstudy/member/domain/MemberDTO.java
+++ src/main/java/com/swger/tddstudy/member/domain/MemberDTO.java
@@ -1,12 +1,19 @@
 package com.swger.tddstudy.member.domain;
 
 import lombok.Getter;
-import lombok.Setter;
 
-@Getter @Setter
+@Getter
 public class MemberDTO {
 
     private String username;
     private String password;
     private String nickname;
+    private String rePassword;
+
+    public MemberDTO(String username, String password, String nickname, String rePassword) {
+        this.username = username;
+        this.password = password;
+        this.nickname = nickname;
+        this.rePassword = rePassword;
+    }
 }
