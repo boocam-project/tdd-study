efb9dc8d8358d12f9aaf19d0f9dfe6064d013d59
JeongUijeong <94631526+JeongUijeong@users.noreply.github.com>
2023-07-31 14:44:19 +0900

    [feat] : 회원가입 양식 검사 추가

--- src/main/java/com/swger/tddstudy/user/restController/UserRestController.java
+++ src/main/java/com/swger/tddstudy/user/restController/UserRestController.java
@@ -1,13 +1,19 @@
 package com.swger.tddstudy.user.restController;
 
 import com.swger.tddstudy.user.domain.UserVO;
+import com.swger.tddstudy.user.service.UserRegex;
 import com.swger.tddstudy.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,25 +21,44 @@ import javax.servlet.http.HttpSession;
 public class UserRestController {
 
     private final UserService userService;
+    private final UserRegex regex;
 
     @PostMapping("/join")
-    public ResponseEntity<UserVO> join(@RequestBody UserVO userVO) {
-        UserVO savedUser = userService.save(userVO);
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(savedUser);
+    public ResponseEntity join(@RequestBody UserVO userVO) {
+        Map<String, Object> message = new HashMap<>();
+        Map<String, Object> test = regex.isJoinRegex(userVO);
+        if ((Boolean) test.get("result")) {
+            UserVO savedUser = userService.save(userVO);
+            message.put("status", 200);
+            message.put("data", savedUser);
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(message);
+        } else {
+            message.put("status", 400);
+            message.put("message", test.get("message"));
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(message);
+        }
     }
 
     @PostMapping("/login")
-    public ResponseEntity<UserVO> login(@RequestBody UserVO userVO, HttpSession session) {
+    public ResponseEntity login(@RequestBody UserVO userVO, HttpSession session) {
+        Map<String, Object> message = new HashMap<>();
         UserVO loginResult = userService.login(userVO);
         if (loginResult != null) {
             session.setAttribute("username", loginResult.getUsername());
             session.setAttribute("id", loginResult.getId());
+            message.put("status", 200);
+            message.put("data", loginResult);
             return ResponseEntity.status(HttpStatus.OK)
-                    .body(loginResult);
+                    .body(message);
         } else {
+            message.put("status", 401);
+            message.put("message", "로그인 실패");
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                    .body(userVO);
+                    .body(message);
         }
     }
 
---
+++ src/main/java/com/swger/tddstudy/user/service/UserRegex.java
@@ -0,0 +1,79 @@
+package com.swger.tddstudy.user.service;
+
+import com.swger.tddstudy.user.domain.UserLevel;
+import com.swger.tddstudy.user.domain.UserType;
+import com.swger.tddstudy.user.domain.UserVO;
+import org.springframework.stereotype.Component;
+
+import javax.xml.transform.Result;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+@Component
+public class UserRegex {
+
+    public boolean isPassword(String str){
+        return Pattern.matches("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,15}$", str);
+    }
+
+    public boolean isUserLevel(String str){
+        for(UserLevel level : UserLevel.values()){
+            if(str.equals(level.name())){
+                return true;
+            };
+        }
+        return false;
+    }
+
+    public boolean isType(String str){
+        for(UserType type : UserType.values()){
+            if(str.equals(type.name())){
+                return true;
+            };
+        }
+        return false;
+    }
+
+    public boolean isFitLength(String str, int min, int max){
+        return Pattern.matches("{"+min+","+max+"}", str);
+    }
+
+    public Map<String, Object> isJoinRegex(UserVO userVO){
+        String message;
+        Map<String, Object> result = new HashMap<>();
+        if(isPassword(userVO.getPassword())){
+            message = "비밀번호는 비밀번호 양식은 영문+특수문자+숫자 8~15자 입니다.";
+            result.put("result", false);
+            result.put("message", message);
+            return result;
+        }
+        if(isUserLevel(userVO.getUserLevel())){
+            message = "유저 레벨은 BRONZE, SILVER, GOLD 가 있습니다.";
+            result.put("result", false);
+            result.put("message", message);
+            return result;
+        }
+        if(isType(userVO.getType())){
+            message = "유저 타입은 USER, ADMIN 이 있습니다.";
+            result.put("result", false);
+            result.put("message", message);
+            return result;
+        }
+        if(isFitLength(userVO.getUsername(),2, 10)){
+            message = "이름을 2~10자 로 입력하십시오.";
+            result.put("result", false);
+            result.put("message", message);
+            return result;
+        }
+        if(isFitLength(userVO.getNickname(),2, 10)){
+            message = "닉네임을 2~10자 로 입력하십시오.";
+            result.put("result", false);
+            result.put("message", message);
+            return result;
+        }
+        result.put("result", true);
+        return result;
+    }
+
+}
