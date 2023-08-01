443c1d5cbcd3bb9faf221b3ab9764cdfc55efcee
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2023-08-01 13:04:42 +0900

    비밀번호 재확인, 아이디,비밀번호 확인 로직 추가

--- src/main/java/com/swger/tddstudy/member/service/MemberService.java
+++ src/main/java/com/swger/tddstudy/member/service/MemberService.java
@@ -1,7 +1,9 @@
 package com.swger.tddstudy.member.service;
 
 import com.swger.tddstudy.member.domain.Member;
-import com.swger.tddstudy.member.domain.Member;
+import com.swger.tddstudy.member.domain.DTO.MemberDTO;
+import com.swger.tddstudy.member.domain.DTO.MemberSignInDTO;
+import com.swger.tddstudy.member.domain.MemberType;
 import com.swger.tddstudy.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,9 +15,23 @@ import javax.transaction.Transactional;
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
-
     public Member join(Member member){
         memberRepository.save(member);
         return member;
+    }
+    public Member SignUp(MemberDTO member){
+        if(!member.getPassword().equals(member.getRePassword())){
+            throw new IllegalArgumentException("RePassword Mismatch");
+        }
+        Member signUpMember = new Member(member.getUsername(), member.getPassword(), member.getNickname());
+        join(signUpMember);
+        return signUpMember;
+    }
+    public Member SignIn(MemberSignInDTO member){
+        Member signInMember = memberRepository.findByUsername(member.getUsername()).orElseThrow(() -> new IllegalArgumentException("Username Mismatch"));
+        if (!signInMember.getPassword().equals(member.getPassword())) {
+            throw new IllegalArgumentException("Password Mismatch");
+        }
+        return signInMember;
     }
 }
