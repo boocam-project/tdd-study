dc6cc46dbf340e2aa78a0525a76929dc2b648eeb
JeongUijeong <94631526+JeongUijeong@users.noreply.github.com>
2023-08-01 14:42:44 +0900

    [test] : Validation 기능 추가를 반영한 test 수정

--- src/test/java/com/swger/tddstudy/user/UserTest.java
+++ src/test/java/com/swger/tddstudy/user/UserTest.java
@@ -4,6 +4,8 @@ import com.swger.tddstudy.user.domain.User;
 import com.swger.tddstudy.user.domain.UserLevel;
 import com.swger.tddstudy.user.domain.UserType;
 import com.swger.tddstudy.user.domain.UserVO;
+import com.swger.tddstudy.user.request.JoinRequest;
+import com.swger.tddstudy.user.request.LoginRequest;
 import com.swger.tddstudy.user.service.UserService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -24,17 +26,16 @@ public class UserTest {
     private UserService userService;
 
     public UserVO newUser() {
-        UserVO user = new UserVO("abc", "abcd1234!", "nickname", "BRONZE", "USER");
-        return user;
+        return new UserVO("abc", "abcd1234!", "nickname", "BRONZE", "USER");
     }
 
     @Test
     @Rollback(value = true)
     @DisplayName("회원가입 테스트 (USER)")
     public void userJoinTest1() {
-        UserVO savedUserVO = userService.save(newUser());
-        assertThat(savedUserVO)
-                .extracting("username", "password", "nickname", "userLevel", "type")
+        JoinRequest joinRequest = new JoinRequest("abc", "abcd1234!", "nickname");
+        UserVO savedUserVO = userService.save(joinRequest);
+        assertThat(savedUserVO).extracting("username", "password", "nickname", "userLevel", "type")
                 .containsExactly("abc", "abcd1234!", "nickname", "BRONZE", "USER");
     }
 
@@ -42,32 +43,31 @@ public class UserTest {
     @Rollback(value = true)
     @DisplayName("회원가입 테스트 (ADMIN)")
     public void userJoinTest2() {
-        UserVO savedUserVO = userService.saveAdmin(newUser());
-        assertThat(savedUserVO)
-                .extracting("username", "password", "nickname", "userLevel", "type")
+        JoinRequest joinRequest = new JoinRequest("abc", "abcd1234!", "nickname");
+        UserVO savedUserVO = userService.saveAdmin(joinRequest);
+        assertThat(savedUserVO).extracting("username", "password", "nickname", "userLevel", "type")
                 .containsExactly("abc", "abcd1234!", "nickname", "BRONZE", "ADMIN");
     }
 
     @Test
     @Rollback(value = true)
     @DisplayName("로그인 테스트")
-    public void userloginTest() {
-        userService.save(newUser());
+    public void userloginTest1() {
+        JoinRequest joinRequest = new JoinRequest("abc", "abcd1234!", "nickname");
+        userService.save(joinRequest);
         // 로그인 객체 생성 후 로그인
-        UserVO loginUserVO = UserVO.builder().username("abc").password("abcd1234!").build();
-        UserVO loginResult = userService.login(loginUserVO);
+        LoginRequest loginRequest = LoginRequest.builder().username("abc").password("abcd1234!").build();
+        UserVO loginResult = userService.login(loginRequest);
         // 로그인 결과가 UserVO면 성공
         assertThat(loginResult).extracting("username", "password", "nickname", "userLevel", "type")
                 .containsExactly("abc", "abcd1234!", "nickname", "BRONZE", "USER");
-
     }
 
     @Test
     @Rollback(value = true)
     @DisplayName("레벨업 테스트 (BRONZE -> SILVER)")
     public void levelUpTest1() {
-        User user = User.builder().id(0L).username("abc").password("abcd1234!").nickname("abcd")
-                .userLevel(UserLevel.BRONZE).type(UserType.USER).build();
+        User user = User.builder().id(0L).username("abc").password("abcd1234!").nickname("abcd").userLevel(UserLevel.BRONZE).type(UserType.USER).build();
         user.levelUp();
         assertThat(user.getUserLevel()).isEqualTo(UserLevel.SILVER);
     }
@@ -76,8 +76,7 @@ public class UserTest {
     @Rollback(value = true)
     @DisplayName("레벨업 테스트 (SILVER -> GOLD)")
     public void levelUpTest2() {
-        User user = User.builder().id(0L).username("abc").password("abcd1234!").nickname("abcd")
-                .userLevel(UserLevel.SILVER).type(UserType.USER).build();
+        User user = User.builder().id(0L).username("abc").password("abcd1234!").nickname("abcd").userLevel(UserLevel.SILVER).type(UserType.USER).build();
         user.levelUp();
         assertThat(user.getUserLevel()).isEqualTo(UserLevel.GOLD);
     }
@@ -86,8 +85,7 @@ public class UserTest {
     @Rollback(value = true)
     @DisplayName("레벨업 테스트 (GOLD -> GOLD)")
     public void levelUpTest3() {
-        User user = User.builder().id(0L).username("abc").password("abcd1234!").nickname("abcd")
-                .userLevel(UserLevel.GOLD).type(UserType.USER).build();
+        User user = User.builder().id(0L).username("abc").password("abcd1234!").nickname("abcd").userLevel(UserLevel.GOLD).type(UserType.USER).build();
         user.levelUp();
         assertThat(user.getUserLevel()).isEqualTo(UserLevel.GOLD);
     }
--- src/test/java/com/swger/tddstudy/user/controller/UserControllerTest.java
+++ src/test/java/com/swger/tddstudy/user/controller/UserControllerTest.java
@@ -2,10 +2,10 @@ package com.swger.tddstudy.user.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.swger.tddstudy.user.domain.UserVO;
+import com.swger.tddstudy.user.request.JoinRequest;
+import com.swger.tddstudy.user.request.LoginRequest;
 import com.swger.tddstudy.user.restController.UserRestController;
-import com.swger.tddstudy.user.service.UserRegex;
 import com.swger.tddstudy.user.service.UserService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +14,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -33,42 +31,18 @@ public class UserControllerTest {
     @MockBean
     UserService userService;
 
-    @MockBean
-    UserRegex userRegex;
-
-    // 회원가입 정규식 확인 결과 HashMap 객체 (True)
-    public Map<String, Object> newRegexTestTrue() {
-        Map<String, Object> regexTest = new HashMap<>();
-        regexTest.put("result", true);
-        //regexTest.put("message", "양식 안맞음");
-        return regexTest;
-    }
-
-    // 회원가입 정규식 확인 결과 HashMap 객체 (False)
-    public Map<String, Object> newRegexTestFalse() {
-        Map<String, Object> regexTest = new HashMap<>();
-        regexTest.put("result", false);
-        regexTest.put("message", "양식에 대한 설명");
-        return regexTest;
-    }
-
     @Test
     @DisplayName("회원가입 테스트 (양식 맞음)")
     void joinTest1() throws Exception {
 
-        UserVO userVO = new UserVO("abc", "abcd1234!", "nickname", "BRONZE", "USER");
-        UserVO savedUserVO = userVO;
-        savedUserVO.setId(0L);
+        JoinRequest joinRequest = new JoinRequest("abc", "abcd1234!", "nickname");
+        UserVO UserVO = new UserVO(0L, "abc", "abcd1234!", "nickname", "BRONZE", "USER");
 
-        given(userService.save(userVO)).willReturn(
-                savedUserVO
+        given(userService.save(joinRequest)).willReturn(
+                UserVO
         );
 
-        given(userRegex.isJoinRegex(userVO)).willReturn(
-                newRegexTestTrue()
-        );
-
-        String json = new ObjectMapper().writeValueAsString(userVO);
+        String json = new ObjectMapper().writeValueAsString(joinRequest);
 
         mockMvc.perform(post("/api/user/join")
                         .content(json)
@@ -82,54 +56,42 @@ public class UserControllerTest {
                 .andExpect(jsonPath("$.data.type").exists())
                 .andDo(print());
 
-        verify(userService).save(userVO);
-
+        verify(userService).save(joinRequest);
     }
 
     @Test
     @DisplayName("회원가입 테스트 (양식 틀림)")
     void joinTest2() throws Exception {
+        JoinRequest joinRequest = new JoinRequest("abc", "abc123", "nickname");
+        UserVO UserVO = new UserVO(0L, "abc", "abc123", "nickname", "BRONZE", "USER");
 
-        UserVO userVO = new UserVO("abc", "abc123", "nickname", "BRONZE", "USER");
-        UserVO savedUserVO = userVO;
-        savedUserVO.setId(0L);
-
-        given(userService.save(userVO)).willReturn(
-                savedUserVO
+        given(userService.save(joinRequest)).willReturn(
+                UserVO
         );
 
-        given(userRegex.isJoinRegex(userVO)).willReturn(
-                newRegexTestFalse()
-        );
-
-        String json = new ObjectMapper().writeValueAsString(userVO);
+        String json = new ObjectMapper().writeValueAsString(joinRequest);
 
         mockMvc.perform(post("/api/user/join")
                         .content(json)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.message").exists())
+                .andExpect(jsonPath("$.error").exists())
                 .andDo(print());
-
-        verify(userRegex).isJoinRegex(userVO);
-
     }
 
 
     @Test
-    @DisplayName("로그인 테스트 (이름, 비밀번호 일치)")
+    @DisplayName("로그인 테스트")
     void loginTest1() throws Exception {
         UserVO userVO = new UserVO(0L, "abc", "abcd1234!", "nickname", "BRONZE", "USER");
 
-        UserVO loginUser = new UserVO();
-        loginUser.setUsername("abc");
-        loginUser.setPassword("abcd1234!");
+        LoginRequest loginRequest = LoginRequest.builder().username("abc").password("abcd1234!").build();
 
-        given(userService.login(loginUser)).willReturn(
+        given(userService.login(loginRequest)).willReturn(
                 userVO
         );
 
-        String json = new ObjectMapper().writeValueAsString(loginUser);
+        String json = new ObjectMapper().writeValueAsString(loginRequest);
 
         mockMvc.perform(post("/api/user/login")
                         .content(json)
@@ -143,32 +105,6 @@ public class UserControllerTest {
                 .andExpect(jsonPath("$.data.type").exists())
                 .andDo(print());
 
-        verify(userService).login(loginUser);
-
-    }
-
-    @Test
-    @DisplayName("로그인 테스트 (이름, 비밀번호 불일치)")
-    void loginTest2() throws Exception {
-
-        UserVO loginUser = new UserVO();
-        loginUser.setUsername("테스트용 이름1");
-        loginUser.setPassword("테스트용 비밀번호2");
-
-        given(userService.login(loginUser)).willReturn(
-                null
-        );
-
-        String json = new ObjectMapper().writeValueAsString(loginUser);
-
-        mockMvc.perform(post("/api/user/login")
-                        .content(json)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isUnauthorized())
-                .andExpect(jsonPath("$.message").exists())
-                .andDo(print());
-
-        verify(userService).login(loginUser);
-
+        verify(userService).login(loginRequest);
     }
 }
