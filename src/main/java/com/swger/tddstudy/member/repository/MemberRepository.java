da5f9e8426d9a815224d4276240c46d637971d39
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2023-08-01 13:05:02 +0900

    NPE 제거를 위해 Optional 사용

--- src/main/java/com/swger/tddstudy/member/repository/MemberRepository.java
+++ src/main/java/com/swger/tddstudy/member/repository/MemberRepository.java
@@ -4,9 +4,10 @@ import com.swger.tddstudy.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
-    List<Member> findByUsername(String  username);
+    Optional<Member> findByUsername(String  username);
 
     @Override
     <S extends Member> S save(S entity);
