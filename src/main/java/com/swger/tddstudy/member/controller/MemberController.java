9a56bad505952457287cb42178ae5daa552f0ff9
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2023-08-01 13:04:19 +0900

    Controller 로직 변경 (Service에서 로직 실행)

--- src/main/java/com/swger/tddstudy/member/controller/MemberController.java
+++ src/main/java/com/swger/tddstudy/member/controller/MemberController.java
@@ -1,15 +1,16 @@
 package com.swger.tddstudy.member.controller;
 
 import com.swger.tddstudy.member.domain.Member;
-import com.swger.tddstudy.member.domain.MemberDTO;
-import com.swger.tddstudy.member.domain.MemberSignInDTO;
+import com.swger.tddstudy.member.domain.DTO.MemberDTO;
+import com.swger.tddstudy.member.domain.DTO.MemberSignInDTO;
 import com.swger.tddstudy.member.repository.MemberRepository;
 import com.swger.tddstudy.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import lombok.experimental.PackagePrivate;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -17,28 +18,17 @@ import java.util.List;
 public class MemberController {
     private final MemberService memberService;
     private final MemberRepository memberRepository;
-//    signIn이 아니라 SignUp이 맞는 표현.. 영어 이슈
-    @PostMapping("/signIn")
-    public String signIn(@Valid @RequestBody MemberDTO memberDTO) {
-        /* 비밀번호 재확인 */
-        if (!memberDTO.getPassword().equals(memberDTO.getRePassword())) {
-            return "Password Mismatch";
-        }
-        Member newMember = new Member(memberDTO.getUsername(),
-                memberDTO.getPassword(), memberDTO.getNickname());
-        memberService.join(newMember);
-        return "signInOK";
+    @PostMapping("/signUp")
+    public String signUp(@Validated @RequestBody MemberDTO memberDTO, BindingResult bindingResult) throws Exception{
+        if (bindingResult.hasErrors()) throw new BindException(bindingResult);
+        memberService.SignUp(memberDTO);
+        return "signUpOK";
     }
 
     @PostMapping("/logIn")
-    public String logIn(@RequestBody MemberSignInDTO memberSignInDTO) {
-        List<Member> byUsername = memberRepository.findByUsername(memberSignInDTO.getUsername());
-        if (byUsername.isEmpty()) {
-            return "Username MisMatch";
-        }
-        if (!byUsername.get(0).getPassword().equals(memberSignInDTO.getPassword())){
-            return "Password MisMatch";
-        }
+    public String logIn(@Validated @RequestBody MemberSignInDTO memberSignInDTO,BindingResult bindingResult) throws Exception{
+        if (bindingResult.hasErrors()) throw new BindException(bindingResult);
+        memberService.SignIn(memberSignInDTO);
         return "LogInOK";
     }
 
