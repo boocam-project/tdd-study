f98dc6d567cd2317cdc267c04a492aeec7470371
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2023-08-02 15:53:18 +0900

    [Day 3-4] 상품 구매-> 재고 -1 메소드 추가 , SellingStatus를 바꾸는 메소드 추가

--- src/main/java/com/swger/tddstudy/product/domain/Product.java
+++ src/main/java/com/swger/tddstudy/product/domain/Product.java
@@ -1,10 +1,10 @@
 package com.swger.tddstudy.product.domain;
 
 import com.swger.tddstudy.util.BaseEntity;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.Id;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,14 +15,36 @@ import lombok.NoArgsConstructor;
 public class Product extends BaseEntity {
 
     @Id
+    @GeneratedValue
     private Long id;
 
+    @NotBlank
     private String name;
 
+    @NotBlank
     private int price;
 
+    @NotBlank
     private int amount;
 
     @Enumerated(EnumType.STRING)
     private SellingStatus sellingStatus;
+
+    public Product(String name, int price, int amount) {
+        this.name = name;
+        this.price = price;
+        this.amount = amount;
+        if(amount >0) this.sellingStatus = SellingStatus.SELLING;
+        else this.sellingStatus = SellingStatus.STOP_SELLING;
+    }
+    public void sellProduct(){
+        this.amount -= 1;
+    }
+    public void sellStop(){
+        this.sellingStatus = SellingStatus.STOP_SELLING;
+    }
+    public void sellStart(){
+        this.sellingStatus = SellingStatus.SELLING;
+    }
+
 }
